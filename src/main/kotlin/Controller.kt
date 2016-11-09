dc91fa07d9b145942e1a62db7939c21f467a88be
Kensuke Tamura <tamura_k@1923.co.jp>
2016-11-09 19:24:04 +0900

    add basic layout component.

--- src/main/kotlin/Controller.kt
+++ src/main/kotlin/Controller.kt
@@ -1,5 +1,25 @@
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.image.ImageView
+import javafx.scene.layout.AnchorPane
+
 /**
  * Created by tamura_k on 2016/11/08.
  */
 class Controller {
+
+    @FXML
+    lateinit var rootPane: AnchorPane
+    @FXML
+    lateinit var weatherImage: ImageView
+    @FXML
+    lateinit var refreshButton: Button
+
+    @FXML
+    private fun initialize() {
+        weatherImage.fitWidthProperty().bind(rootPane.widthProperty())
+        weatherImage.fitHeightProperty().bind(rootPane.heightProperty())
+    }
+
+
 }
